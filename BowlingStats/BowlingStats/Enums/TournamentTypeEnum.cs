3c669bca1061973872a8f83290c4ed6d98590082
detector90 <nicolalacavalla@libero.it>
2023-02-26 20:02:28 +0100

    Fixed tournament types labels

--- BowlingStats/BowlingStats/Enums/TournamentTypeEnum.cs
+++ BowlingStats/BowlingStats/Enums/TournamentTypeEnum.cs
@@ -9,9 +9,11 @@ namespace BowlingStats.Enums
     {
         [Description("Allenamento")]
         Training = 0,
-        [Description("Torneo ufficiale")]
-        OfficialEvent = 1,
-        [Description("Torneo ufficiale interno")]
-        InternalOfficialEvent = 2
+        [Description("Torneo federale")]
+        FederalEvent = 1,
+        [Description("Torneo volante")]
+        SociableEvent = 2,
+        [Description("Torneo monetario")]
+        MonetaryEvent = 3
     }
 }
